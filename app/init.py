ff44e0da0724a8bd33ca4b6030959528b44672d2
Zeque3112 <crecyer@yahoo.com>
2026-06-02 16:24:04 -0300

    feat: modifico config y agrego a init

--- app/init.py
+++ app/init.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+from app.config import Config
 from app.routes.health import health_bp
 from app.routes.users import user_bp
 
 def create_app():
     app = Flask(__name__)
     
+    app.config.from_object(Config)
+
     app.register_blueprint(health_bp)
     app.register_blueprint(user_bp)
     
