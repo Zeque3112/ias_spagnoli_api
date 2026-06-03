1cb130d9489fdd2c763ed4336a63bd20f748f2a9
Zeque3112 <crecyer@yahoo.com>
2026-06-02 21:06:23 -0300

    feat: agrego chekeo de health de la db

--- app/routes/health.py
+++ app/routes/health.py
@@ -1,11 +1,25 @@
 from datetime import datetime
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
+from sqlalchemy import text
+from app.db import db
 
 health_bp = Blueprint('health', __name__)
 
 @health_bp.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({'status': 'ok',
-                    "service": "ias_apellido_api",
-                    "timestamp": datetime.now()
-                    }), 200
+
+    try:
+        db.session.execute(text("SELECT 1"))
+        database_status = "up"
+
+    except Exception:
+        database_status = "down"
+
+
+    return jsonify({
+        "status": "ok",
+        "service": "ias_spagnoli_api",
+        "environment": current_app.config["ENV"],
+        "database": database_status,
+        "timestamp": datetime.now().isoformat()
+    }), 200
