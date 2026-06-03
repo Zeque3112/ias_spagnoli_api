665ab69b088f4b8d73d530597b4767b05f9c14fa
Zeque3112 <crecyer@yahoo.com>
2026-06-02 21:05:40 -0300

    feat: diferencia entre config para app y para test

--- app/config.py
+++ app/config.py
@@ -6,3 +6,10 @@ class Config:
     # ENV = os.getenv('ENV', 'production')
     ENV = os.getenv('ENV', 'development')
     DEBUG = os.getenv('DEBUG', 'False').lower() == "true"
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
